4002852459746f5114e3d1f2da7abaf59ffb6408
Josh Massa <joshmassa@gmail.com>
2023-12-16 19:59:50 -0500

    pushing mini project from local machine to remote repository

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,8 @@
+// Script for button functionality 
+document.addEventListener("DOMContentLoaded", function() {
+var button = document.getElementById("button");
+button.addEventListener("click", function() {
+    alert("Form Submitted!");
+    });
+});
+// Script end
